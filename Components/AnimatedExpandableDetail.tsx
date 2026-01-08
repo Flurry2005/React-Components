d113d487b2a61286451dda1ef00adea35c283c5c
Flurry <41346632+Flurry2005@users.noreply.github.com>
2026-01-08 21:16:55 +0100

    Added bordered mode

--- Components/AnimatedExpandableDetail.tsx
+++ Components/AnimatedExpandableDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 {
   /* Make sure to copy custom variant 'details-open-sibling' from index.css */
@@ -7,35 +7,40 @@ import React, { useState } from "react";
 interface AnimatedExpandableDetailProps {
   title: string;
   children: React.ReactNode;
-  width: string;
-  height: string;
-  border?: boolean;
-  bannerColor?: string;
-  contentColor?: string;
-  textColor?: string;
-  titleColor?: string;
+  tswidth: string;
+  tsheight: string;
+  tsborder?: string;
+  tsborderColor?: string;
+  tsbannerColor?: string;
+  tscontentColor?: string;
+  tstextColor?: string;
+  tstitleColor?: string;
 }
 
 function AnimatedExpandableDetail({
   title,
   children,
-  width,
-  height,
-  border = false,
-  bannerColor = "bg-slate-900",
-  contentColor = "bg-slate-900",
-  textColor = "text-white",
-  titleColor = "text-white",
+  tswidth,
+  tsheight,
+  tsborder = "border-0",
+  tsbannerColor = "bg-slate-900",
+  tscontentColor = "bg-slate-900",
+  tstextColor = "text-white",
+  tstitleColor = "text-white",
+  tsborderColor = "text-white",
 }: AnimatedExpandableDetailProps) {
   const [isOpen, setIsOpen] = useState(false);
-
   return (
     <article>
       <details
-        style={{ maxWidth: `${width}` }}
+        style={{ maxWidth: `${tswidth}` }}
         className={
           "mt-2 rounded-md transition-[border-radius] duration-50 ease-out " +
-          bannerColor
+          tsbannerColor +
+          " " +
+          tsborderColor +
+          " " +
+          tsborder
         }
         onToggle={(e) => {
           const el = e.currentTarget;
@@ -53,24 +58,30 @@ function AnimatedExpandableDetail({
       >
         <summary className="[&::-webkit-details-marker]:hidden block select-none">
           <span className="flex justify-between items-center pr-2 pl-[0.7rem] text-[1rem]">
-            <span className="relative flex items-center bg-transparent h-12 text-white hover:cursor-pointer details-span">
+            <span className="relative flex items-center bg-transparent h-12 hover:cursor-pointer details-span">
               <i
                 style={{ rotate: isOpen ? "90deg" : "0deg" }}
-                className="flex items-center mr-2 text-xl transition-[rotate] duration-400 ease-out fa-caret-right fa-solid"
+                className={
+                  "flex items-center mr-2 text-xl transition-[rotate] duration-400 ease-out fa-caret-right fa-solid " +
+                  tstextColor
+                }
               ></i>
-              <h1 className={titleColor}>{title}</h1>
+              <h1 className={tstitleColor}>{title}</h1>
             </span>
           </span>
         </summary>
       </details>
-      {border ? (
+      {tsborder !== "border-0" ? (
         <div
           style={{
-            maxWidth: `${width}`,
-            maxHeight: isOpen ? `${height}` : "0px",
+            maxWidth: `${tswidth}`,
+            maxHeight: isOpen ? `${tsheight}` : "0px",
           }}
           className={
-            "box-border bg-slate-900 border-0 px-2.5 overflow-hidden rounded-b-md text-white transition-[max-height,border-color] duration-400 ease-out details-open-sibling:border-l-2 details-open-sibling:border-r-2 details-open-sibling:border-b-2 details-open-sibling:border-green-500"
+            "border-0 px-2.5 overflow-hidden rounded-b-md transition-[max-height,border-color,border-width] duration-400 ease-out details-open-sibling:border-l details-open-sibling:border-r details-open-sibling:border-b not-details-open-sibling:border-gray-100! " +
+            tscontentColor +
+            " " +
+            tsborderColor
           }
         >
           {children}
@@ -78,15 +89,15 @@ function AnimatedExpandableDetail({
       ) : (
         <div
           style={{
-            maxWidth: `${width}`,
-            maxHeight: isOpen ? `${height}` : "0px",
+            maxWidth: `${tswidth}`,
+            maxHeight: isOpen ? `${tsheight}` : "0px",
           }}
           className={
-            "box-borderborder-0 px-2.5 overflow-hidden rounded-b-md text-white transition-[max-height] duration-400 ease-out " +
-            contentColor
+            "box-border border-0 px-2.5 overflow-hidden rounded-b-md transition-[max-height] duration-400 ease-out " +
+            tscontentColor
           }
         >
-          <p className={textColor}>{children}</p>
+          <p className={tstextColor}>{children}</p>
         </div>
       )}
     </article>
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import "./App.css";
 import AnimatedExpandableDetail from "../Components/AnimatedExpandableDetail";
 
+/* Add to workspace settings for intellisense on properties
+"tailwindCSS.experimental.classRegex": [
+    [
+      "\\bts[A-Za-z0-9_]*\\s*=\\s*([\"'`][^\"'`]*[\"'`])",
+      "[\"'`]([^\"'`]*)[\"'`]"
+    ]
+  ] 
+*/
+
 function App() {
   return (
     <>
       <div className="flex p-[10%] w-full h-full">
         <AnimatedExpandableDetail
           title="Animated Detail"
-          width="200px"
-          height="200px"
-          bannerColor="bg-neutral-600"
-          contentColor="bg-neutral-200"
-          textColor="text-black"
+          tswidth="200px"
+          tsheight="200px"
+          tsbannerColor="bg-neutral-000"
+          tscontentColor="bg-neutral-100"
+          tstitleColor="text-black"
+          tstextColor="text-black"
+          tsborderColor="border-neutral-400"
+          tsborder="border"
         >
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet at
           soluta nesciunt vero dolore qui tempora porro nulla maxime magnam!
